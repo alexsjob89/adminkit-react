1b81544acf90ced46c4b0fc2630c0e1fe3a39867
alexsjob89 <103150434+alexsjob89@users.noreply.github.com>
2023-03-08 09:12:20 +0000

    test

--- src/components/worldsElemnts/Britain.jsx
+++ src/components/worldsElemnts/Britain.jsx
@@ -3,11 +3,7 @@ import React from "react";
 function Britain() {
   return (
     <div className="britain ">
-      <div className="britain-content">
-        <img src="./images/black-watch.jpg" alt="" style={{ width: "100px" }} />
-        <h1 className="brit-title">Stepping back does make Sussex safer and at peace</h1>
-        <span className="years">by Salman2 years ago</span>
-        <p className="brit-para">You audience. Least, the recently his repeat the this avarice for the have and was</p>
+
       </div>
     </div>
   );
